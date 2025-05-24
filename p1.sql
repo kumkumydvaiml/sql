d5b1a3b07c755d204870ef066ce7ff5b63c01d84
kumkumyadav780 <Kumkumyadav8841@gmail.com>
2025-05-24 13:13:41 +0530

    sql practice

--- p1.sql
+++ p1.sql
@@ -1,10 +1,16 @@
-create database ed;
-use ed;
-drop database ed;
 create database db40;
 use db40;
+show tables;
 create table department (dept_id int primary key,
 dept_name varchar(50) not null);
+alter table department modify dept_name varchar(50);
+alter table department rename to dept;
+select * from department;
+describe department;
+alter table dept rename to department;
+
+
+
 
 
 create table employee (emp_id int primary key,
