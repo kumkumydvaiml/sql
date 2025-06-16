dcfc255420be02f183abf55096075c0bae5c0386
kumkumyadav780 <Kumkumyadav8841@gmail.com>
2025-06-16 23:42:58 +0530

    sql

--- count aggregate function.sql
+++ count aggregate function.sql
@@ -1,8 +1,32 @@
 use dummy;
+select * from customers;
 
-show tables;
 
-select * from offices;
+select * from customers city limit 3;
 
-select city,count(country) from offices group by (city);
+select * from customers order by creditlimit desc limit 3;
+
+select * from students;
+
+delete from students where student_number=5;
+
+alter table student rename to students;
+
+desc students;
+
+alter table students drop primary key;
+
+alter table students add column age int not null;
+
+update students set age=20 where student_number=1;
+
+select * from students where marks>80;
+select * from students where marks>70 and dept="math";
+select * from students where marks between 80 and 90;
+
+
+
+
+
+
 
